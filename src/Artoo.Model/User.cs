5a7cb0c27688f06f468d73139e81a7d1a7987229
Robert Silvie <robert_nsu@hotmail.com>
2021-07-12 22:54:25 -0500

    Initial commit

---
+++ src/Artoo.Model/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Artoo.Model
+{
+    using System;
+
+    /// <summary>
+    /// User class.
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ src/Artoo.Model/UserAccount.cs
@@ -0,0 +1,25 @@
+namespace Artoo.Model
+{
+    using System;
+
+    public class UserAccount
+    {
+        public int Id { get; set; }
+
+        public string Phone { get; set; }
+        
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+    }
+}
